6434fe49bd2b372781be0be50d85aaa90d246783
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2020-08-02 16:17:52 -0500

    Working babel

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ const browserify = require('browserify');
 const babelify = require('babelify');
 const source = require('vinyl-source-stream');
 const uglify = require('gulp-uglify');
-const buffer      = require('vinyl-buffer');
+const buffer = require('vinyl-buffer');
+const sourcemaps = require('gulp-sourcemaps');
 
 babelify.configure({
     babelrc: './.babelrc'
@@ -15,6 +16,8 @@ gulp.task('build', function () {
         .bundle()
         .pipe(source('script.js'))
         .pipe(buffer())
+        .pipe(sourcemaps.init())
         .pipe(uglify())
+        .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest('client-build'));
 });
